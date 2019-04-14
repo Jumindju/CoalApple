842d5d320cf7db07af8294910eabedde203c5b2a
jumindju <jumindju@googlemail.com>
2019-04-14 22:51:19 +0200

    Create window in main for testing

---
+++ Sources/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#define GLEW_STATIC
+#define SDL_MAIN_HANDLED
+#include "Context.h"
+#include "Config.h"
+using namespace std;
+
+int main()
+{
+	Config config;
+	//config.isFullScreen = true;
+	Context context(config);
+
+	return 0;
+}
